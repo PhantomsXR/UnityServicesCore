326900fb130716d2c902d77d1576e097eb80fdf7
PhantomsXR <phantomxm2021@gmail.com>
2024-11-25 14:52:45 +0800

    Add RegionChecking

---
+++ Runtime/Core/CheckRegion.cs
@@ -0,0 +1,47 @@
+// // /*===============================================================================
+// // Copyright (C) 2024 PhantomsXR Ltd. All Rights Reserved.
+// //
+// // This file is part of the Unity.Services.Core.
+// //
+// // The Infiniplay_visionOS cannot be copied, distributed, or made available to
+// // third-parties for commercial purposes without written permission of PhantomsXR Ltd.
+// //
+// // Contact info@phantomsxr.com for licensing requests.
+// // ===============================================================================*/
+
+using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Unity.Services.Core
+{
+    public class CheckRegion : MonoBehaviour
+    {
+        public static IpInfo IpInfo;
+        public static bool IsChina;
+
+        static async void Checking()
+        {
+            using HttpClient tmp_Client = new HttpClient();
+            string tmp_APIUrl = "https://ip-api.com";
+            HttpResponseMessage tmp_Response = await tmp_Client.GetAsync(tmp_APIUrl);
+            if (!tmp_Response.IsSuccessStatusCode) return;
+            string tmp_JsonResponse = await tmp_Response.Content.ReadAsStringAsync();
+            IpInfo = JsonConvert.DeserializeObject<IpInfo>(tmp_JsonResponse);
+            IsChina = IpInfo.country == "China";
+            Debug.Log(IpInfo.country);
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void DoCheck()
+        {
+            Checking();
+        }
+    }
+
+    public class IpInfo
+    {
+        public string country;
+    }
+}
