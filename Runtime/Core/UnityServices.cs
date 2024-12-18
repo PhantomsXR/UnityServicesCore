a5f678b0ee7ea4636fa271f5a36e4961e45f936f
PhantomsXR <phantomxm2021@gmail.com>
2024-12-18 21:52:42 +0800

    Update 1.14.0
    ## [1.14.0] - 2024-11-06
    
    ### Added
    
    - Added `ServicesInitialization` monobehaviour to manage services in the scene authoring.
    - Added `ServicesBehaviour` abstract monobehaviour as a base for behaviours built on top of services.
    - Added unique identifier for custom service registry creation (`UnityServices.CreateServices`).
    - Added custom services registries dictionary in `UnityServices.Services`.
    - Added `GetIdentifier()` method to `IUnityServices` interface with a default implementation.
    
    ### Changed
    
    - Updated the minimum supported Editor version to 2021.3.
    
    ## [1.13.1] - 2024-10-17
    
    ### Added
    
    - Refresh button for Environment dropdown in Project Settings > Services > Environments menu.
    
    ### Fixed
    
    - Fixed user permissions fetching for cloud projects where the user has the 'inherited from organization' role.
    
    ### Changed
    
    - The EnvironmentProvider, EnvironmentSaveSystem, EnvironmentService, and related interfaces have
      been updated to support an object representing the environment (name and id) instead of a string
      representing the environment name.

--- Runtime/Core/UnityServices.cs
+++ Runtime/Core/UnityServices.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -15,20 +16,8 @@ namespace Unity.Services.Core
         /// </summary>
         public static event Action Initialized
         {
-            add
-            {
-                if (Instance != null)
-                {
-                    Instance.Initialized += value;
-                }
-            }
-            remove
-            {
-                if (Instance != null)
-                {
-                    Instance.Initialized -= value;
-                }
-            }
+            add { if (Instance != null) { Instance.Initialized += value; } }
+            remove { if (Instance != null) { Instance.Initialized -= value; } }
         }
 
         /// <summary>
@@ -36,20 +25,8 @@ namespace Unity.Services.Core
         /// </summary>
         public static event Action<Exception> InitializeFailed
         {
-            add
-            {
-                if (Instance != null)
-                {
-                    Instance.InitializeFailed += value;
-                }
-            }
-            remove
-            {
-                if (Instance != null)
-                {
-                    Instance.InitializeFailed -= value;
-                }
-            }
+            add { if (Instance != null) { Instance.InitializeFailed += value; } }
+            remove { if (Instance != null) { Instance.InitializeFailed -= value; } }
         }
 
         /// <summary>
@@ -57,8 +34,14 @@ namespace Unity.Services.Core
         /// </summary>
         public static IUnityServices Instance { get; set; }
 
+        /// <summary>
+        /// The custom instances of unity services.
+        /// </summary>
+        public static IReadOnlyDictionary<string, IUnityServices> Services => s_Services;
         internal static TaskCompletionSource<object> InstantiationCompletion { get; set; }
         internal static ExternalUserIdProperty ExternalUserIdProperty = new ExternalUserIdProperty();
+
+        static Dictionary<string, IUnityServices> s_Services { get; } = new Dictionary<string, IUnityServices>();
 
         /// <summary>
         /// Initialization state.
@@ -69,8 +52,7 @@ namespace Unity.Services.Core
             {
                 if (!UnityThreadUtils.IsRunningOnUnityThread)
                 {
-                    throw new ServicesInitializationException(
-                        "You are attempting to access UnityServices.State from a non-Unity Thread." +
+                    throw new ServicesInitializationException("You are attempting to access UnityServices.State from a non-Unity Thread." +
                         " UnityServices.State can only be accessed from Unity Thread");
                 }
 
@@ -122,26 +104,21 @@ namespace Unity.Services.Core
         /// <returns>
         /// Return a handle to the initialization operation.
         /// </returns>
-        [PreserveDependency("Register()", "Unity.Services.Core.Registration.CorePackageInitializer",
-            "Unity.Services.Core.Registration")]
-        [PreserveDependency("CreateStaticInstance()", "Unity.Services.Core.Internal.UnityServicesInitializer",
-            "Unity.Services.Core.Internal")]
-        [PreserveDependency("EnableServicesInitializationAsync()",
-            "Unity.Services.Core.Internal.UnityServicesInitializer", "Unity.Services.Core.Internal")]
+        [PreserveDependency("Register()", "Unity.Services.Core.Registration.CorePackageInitializer", "Unity.Services.Core.Registration")]
+        [PreserveDependency("CreateStaticInstance()", "Unity.Services.Core.Internal.UnityServicesInitializer", "Unity.Services.Core.Internal")]
+        [PreserveDependency("EnableServicesInitializationAsync()", "Unity.Services.Core.Internal.UnityServicesInitializer", "Unity.Services.Core.Internal")]
         [PreserveDependency("CaptureUnityThreadInfo()", "Unity.Services.Core.UnityThreadUtils", "Unity.Services.Core")]
         public static async Task InitializeAsync(InitializationOptions options)
         {
             if (!UnityThreadUtils.IsRunningOnUnityThread)
             {
-                throw new ServicesInitializationException(
-                    "You are attempting to initialize Unity Services from a non-Unity Thread." +
+                throw new ServicesInitializationException("You are attempting to initialize Unity Services from a non-Unity Thread." +
                     " Unity Services can only be initialized from Unity Thread");
             }
 
             if (!Application.isPlaying)
             {
-                throw new ServicesInitializationException(
-                    "You are attempting to initialize Unity Services in Edit Mode." +
+                throw new ServicesInitializationException("You are attempting to initialize Unity Services in Edit Mode." +
                     " Unity Services can only be initialized in Play Mode");
             }
 
@@ -155,17 +132,52 @@ namespace Unity.Services.Core
                 await InstantiationCompletion.Task;
             }
 
-            await CheckRegion.Checking();
             await Instance.InitializeAsync(options);
+        }
+
+        /// <summary>
+        /// Create a new services registry. Uses a Guid as identifier.
+        /// </summary>
+        /// <exception cref="ServicesCreationException">
+        /// Thrown when the services registry could not be created.
+        /// </exception>
+        /// <returns>The services registry instance</returns>
+        public static IUnityServices CreateServices()
+        {
+            return CreateServices(Guid.NewGuid().ToString());
         }
 
         /// <summary>
         /// Create a new services registry.
         /// </summary>
+        /// <param name="servicesId">Unique identifier for the services registry. Defaults to a GUID.</param>
+        /// <exception cref="ArgumentException">
+        /// Thrown when the services identifier is invalid.
+        /// </exception>
+        /// <exception cref="ServicesCreationException">
+        /// Thrown when the services registry could not be created.
+        /// </exception>
         /// <returns>The services registry instance</returns>
-        public static IUnityServices CreateServices()
+        public static IUnityServices CreateServices(string servicesId)
         {
-            return UnityServicesBuilder.Create();
+            if (string.IsNullOrEmpty(servicesId))
+            {
+                throw new ArgumentException($"The services identifier cannot be null or empty");
+            }
+
+            if (s_Services.ContainsKey(servicesId))
+            {
+                throw new ServicesCreationException($"The services identifier '{servicesId}' is already registered.");
+            }
+
+            var services = UnityServicesBuilder.Create(servicesId);
+            s_Services[servicesId] = services;
+            return services;
+        }
+
+        internal static void ClearServices()
+        {
+            s_Services.Clear();
         }
     }
 }
